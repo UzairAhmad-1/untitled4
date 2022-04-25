ac66702347710e9188166df462963884071226b3
seangeri <seanse97@gmail.com>
2022-04-25 20:03:51 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Departments} from "./components/Departments";
+
+function App() {
+  return (
+      <div className={"container"}>
+        <Departments/>
+      </div>
+  );
+}
+
+export default App;
